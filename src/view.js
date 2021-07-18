060b846abd72f3b32590f2ec483069feb41161c3
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-19 01:32:57 +0200

    Format view.js

--- src/view.js
+++ src/view.js
@@ -1,52 +1,55 @@
-const readline = require('readline');
-const util = require('util');
+/* eslint-disable no-console */
+const readline = require('readline')
+const util = require('util')
 
 module.exports = class View {
   // terminal color references
-  static white = '\x1b[0m';
-  static red = '\x1b[31m';
-  static green = '\x1b[32m';
+  static white = '\x1b[0m'
+
+  static red = '\x1b[31m'
+
+  static green = '\x1b[32m'
 
   constructor() {
     this.rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
-    });
-    this.question = util.promisify(this.rl.question).bind(this.rl);
+    })
+    this.question = util.promisify(this.rl.question).bind(this.rl)
   }
 
   display(message) {
-    console.log(`${message}`);
+    console.log(`${message}`)
   }
 
   displayGreen(message) {
-    console.log(View.green, `${message}`, View.white);
+    console.log(View.green, `${message}`, View.white)
   }
 
   displayError(err) {
-    console.log(View.red, `\n *** ${err} ***\n`, View.white);
+    console.log(View.red, `\n *** ${err} ***\n`, View.white)
   }
 
   async askRoutesFolder() {
-    this.displayGreen('Provide your routes folder by editing below line:');
-    this.rl.write(`${process.cwd()}/src/routes`);
+    this.displayGreen('Provide your routes folder by editing below line:')
+    this.rl.write(`${process.cwd()}/src/routes`)
 
-    const path = await this.question('>');
-    return path;
+    const path = await this.question('>')
+    return path
   }
 
   async askForExecution() {
-    this.display('\nClick any key to continue... (CTRL+C for Exit)');
-    process.stdin.setRawMode(true);
-    return new Promise((resolve) =>
+    this.display('\nClick any key to continue... (CTRL+C for Exit)')
+    process.stdin.setRawMode(true)
+    return new Promise(resolve =>
       process.stdin.once('data', () => {
-        process.stdin.setRawMode(false);
-        resolve();
+        process.stdin.setRawMode(false)
+        resolve()
       })
-    );
+    )
   }
 
   endExecution() {
-    this.rl.close();
+    this.rl.close()
   }
-};
+}
