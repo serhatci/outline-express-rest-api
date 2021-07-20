bca263004ee564906b988b1ee25482e1f8fa36a5
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-20 18:46:40 +0200

    Add getRouteFilePaths method

--- src/fileManager.js
+++ src/fileManager.js
@@ -1,6 +1,13 @@
 const fs = require('fs')
 
 module.exports = class FileManager {
+  async getRouteFilePaths(userInput) {
+    if (!fs.existsSync(userInput)) throw Error(`${userInput} does not exist!`)
+
+    if (/\..*?$/.test(userInput)) return [userInput]
+
+    const jsFiles = this.getJsFilesInFolder(userInput)
+    return jsFiles.map(file => `${userInput}/${file}`)
   }
 
   getJsFilesInFolder(path) {
