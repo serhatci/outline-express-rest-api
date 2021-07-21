b047ffb67bb962b40983782742f0ef86077e2916
serhat <36408498+serhatci@users.noreply.github.com>
2021-07-22 00:01:33 +0200

    Carry index file to src folder

---
+++ src/index.js
@@ -0,0 +1,33 @@
+/* eslint-disable global-require */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable import/no-dynamic-require */
+
+const View = require('./view')
+const FileManager = require('./file-manager')
+const TextManager = require('./text-manager')
+
+const view = new View()
+const fileManager = new FileManager()
+const textManager = new TextManager()
+
+async function init() {
+  try {
+    const userInput = textManager.cleanInput(process.argv.slice(2)[0])
+
+    const routeFilePaths = await fileManager.getRouteFilePaths(userInput)
+    if (routeFilePaths.length > 1) await view.displayTestedFiles(routeFilePaths)
+
+    routeFilePaths.forEach(filePath => {
+      const fullPath = fileManager.getFullPath(filePath)
+      const routeObj = require(fullPath)
+      const results = textManager.outlineRouteObj(routeObj)
+
+      const filename = textManager.getFilename(filePath)
+      view.displayResults(results, filename)
+    })
+  } catch (err) {
+    view.displayError(err)
+  }
+}
+
+module.exports = init
