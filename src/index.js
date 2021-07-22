d3d6fb0c8d11c5b04e5523de0b9636257d87670e
Serhat Ciftci <36408498+serhatci@users.noreply.github.com>
2021-07-22 19:19:23 +0200

    Implement extraFeature process argv handling

--- src/index.js
+++ src/index.js
@@ -12,15 +12,16 @@ const textManager = new TextManager()
 
 async function init() {
   try {
-    const userInput = textManager.cleanInput(process.argv.slice(2)[0])
+    const userGivenPath = textManager.cleanInput(process.argv.slice(2)[0])
+    const extraFeature = textManager.evaluateExtraFeature(process.argv.slice(2)[1])
 
-    const routeFilePaths = await fileManager.getRouteFilePaths(userInput)
+    const routeFilePaths = await fileManager.getRouteFilePaths(userGivenPath)
     if (routeFilePaths.length > 1) await terminal.displayTestedFiles(routeFilePaths)
 
     routeFilePaths.forEach(filePath => {
       const fullPath = fileManager.getFullPath(filePath)
       const routeObj = require(fullPath)
-      const results = textManager.outlineRouteObj(routeObj)
+      const results = textManager.outlineRouteObj(routeObj, extraFeature)
 
       const filename = textManager.getFilename(filePath)
       terminal.displayResults(results, filename)
